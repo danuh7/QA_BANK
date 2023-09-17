ca202982a85c20757a9a23c46c2eceaa868580dc
Emmanuel Valentin <ema_110@outlook.es>
2023-09-16 21:05:06 -0600

    Se añadio página validar-cuenta

--- cajero_web/views.py
+++ cajero_web/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
 def index(request):
     return render(request, "cajero_web/index.html")
 
 def actualizar_nip(request):
     return render(request, "cajero_web/actualizar-nip.html")
+
+def validar_cuenta(request):
+    return render(request, "cajero_web/validar-cuenta.html")
