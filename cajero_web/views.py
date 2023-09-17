d0e7f1461043374b1d36b64eba880828cef3fce4
Emmanuel Valentin <ema_110@outlook.es>
2023-09-16 23:18:58 -0600

    views.py: Formateo del código

--- cajero_web/views.py
+++ cajero_web/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def index(request):
     return render(request, "cajero_web/index.html")
 
+
 def actualizar_nip(request):
     return render(request, "cajero_web/actualizar-nip.html")
+
 
 def validar_cuenta(request):
     return render(request, "cajero_web/validar-cuenta.html")
 
+
 def realizar_pago(request):
     return render(request, "cajero_web/realizar-pago.html")
+
+
 def operacion_exitosa(request):
     return render(request, "cajero_web/operacion-exitosa.html")
