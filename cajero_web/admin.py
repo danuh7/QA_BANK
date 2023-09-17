cfda8e44a2fb85d6923896288ff8e3ccf8f29076
Emmanuel Valentin <ema_110@outlook.es>
2023-09-17 17:32:33 -0600

    Se añadieron models al panel admin

--- cajero_web/admin.py
+++ cajero_web/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Cliente, Cuenta, Operacion
 
-# Register your models here.
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Cuenta)
+class CuentaAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Operacion)
+class OperacionAdmin(admin.ModelAdmin):
+    pass
