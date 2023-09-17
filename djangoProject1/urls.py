f11229ef9614df0c490da9888177b107aebf24a5
Emmanuel Valentin <ema_110@outlook.es>
2023-09-16 18:56:00 -0600

    Se renombró proyecto de cajero_simulador a djangoProject1

--- djangoProject1/urls.py
+++ djangoProject1/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for cajero_simulador project.
+URL configuration for djangoProject1 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
