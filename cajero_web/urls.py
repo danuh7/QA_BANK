9fec0bf4a0e502e953c46ff63951b433b0f9ac85
Emmanuel Valentin <ema_110@outlook.es>
2023-09-18 00:57:41 -0600

    Se actualizaron las rutas

--- cajero_web/urls.py
+++ cajero_web/urls.py
@@ -1,12 +1,13 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('actualizar-nip', views.actualizar_nip, name='actualizar-nip'),
-    path('validar-cuenta', views.validar_cuenta, name='validar-cuenta'),
-    path('realizar-pago', views.realizar_pago, name='realizar-pago'),
-    path('operacion-exitosa', views.operacion_exitosa, name='operacion-exitosa'),
-    path('consultar-saldo', views.consultar_saldo, name="consultar-saldo"),
-    path('retirar-saldo', views.retirar_saldo, name="retirar-saldo")
+    path('actualizar-nip/', views.actualizar_nip, name='actualizar-nip'),
+    path('validar-cuenta/', views.validar_cuenta, name='validar-cuenta'),
+    re_path(r'^validar-cuenta/(?P<view>\w)/$', views.validar_cuenta),
+    path('realizar-pago/', views.realizar_pago, name='realizar-pago'),
+    path('operacion-exitosa/<int:operacion_id>', views.operacion_exitosa, name='operacion-exitosa'),
+    path('consultar-saldo/', views.consultar_saldo, name="consultar-saldo"),
+    path('retirar-saldo/', views.retirar_saldo, name="retirar-saldo")
 ]
--- cajero_web/views.py
+++ cajero_web/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Operacion
 
 
 # Create your views here.
@@ -12,15 +13,30 @@ def actualizar_nip(request):
 
 
 def validar_cuenta(request):
-    return render(request, "cajero_web/validar-cuenta.html")
+    next_page = request.GET.get('redirect')
+
+    if next_page == None:
+        next_page = ''
+
+    return render(request, "cajero_web/validar-cuenta.html", {
+        "next_page": next_page,
+    })
 
 
 def realizar_pago(request):
     return render(request, "cajero_web/realizar-pago.html")
 
 
-def operacion_exitosa(request):
-    return render(request, "cajero_web/operacion-exitosa.html")
+def operacion_exitosa(request, operacion_id):
+    operacion = get_object_or_404(Operacion, pk=operacion_id)
+    return render(request, "cajero_web/operacion-exitosa.html", {
+        "fecha": operacion.fecha,
+        "hora": operacion.hora,
+        "monto": operacion.monto,
+        "tipo_operacion": str(operacion.id_tipo_operacion),
+        "cuenta_origen": str(operacion.id_origen),
+        "cuenta_destino": str(operacion.id_destino),
+    })
 
 
 def consultar_saldo(request):
